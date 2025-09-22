c26f25b4bb566fb28277d068bd2b0cc77d6b3d56
Aitor <agj0034@alu.medac.es>
2025-09-22 20:07:25 +0200

    first commit

--- DES/Tema1/juego.php
+++ DES/Tema1/juego.php
@@ -1,3 +1,10 @@
 <?php
-echo "Hola, mundo!";
+$contador = 0;
+$numero = 13;
+for ($i = 1; $i <= $numero; $i++) {
+    if ($i % 2 == 0 && $i % 3 == 0) {
+        $contador++;
+    }
+}
+echo $contador;
 ?>

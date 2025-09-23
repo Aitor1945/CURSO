6cfb0a83f8781009a2c74ee74963d151a6090b48
Aitor <agj0034@alu.medac.es>
2025-09-23 17:30:15 +0200

    first commit

---
+++ DES/Tema1/Ajedrez.php
@@ -0,0 +1,16 @@
+<?php
+$n = 8;
+
+for ($i = 0; $i < $n; $i++) {
+    for ($j = 0; $j < $n; $j++) {
+        if (($i + $j) % 2 == 0) {
+            echo "B ";
+        } else {
+            echo "N ";
+        }
+
+    }
+    echo "\n";
+}
+
+?>

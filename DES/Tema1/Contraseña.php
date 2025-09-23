6771e0b01370de2c747eff276bae89635d19c6d7
Aitor <agj0034@alu.medac.es>
2025-09-23 17:01:30 +0200

    first commit

--- DES/Tema1/Contraseña.php
+++ DES/Tema1/Contraseña.php
@@ -4,26 +4,26 @@ function validar_contrasena($contrasena, &$errores) {
     $errores = "";
 
     if (strlen($contrasena) < 8) {
-        $errores = "Debe tener al menos 8 caracteres";
+        $errores .= "Debe tener al menos 8 caracteres. ";
     }
-    elseif (!preg_match('/[a-z]/', $contrasena)) {
-        $errores = "Debe contener al menos una letra minúscula";
+    if (!preg_match('/[a-z]/', $contrasena)) {
+        $errores .= "Debe contener al menos una letra minúscula. ";
     }
-    elseif (!preg_match('/[A-Z]/', $contrasena)) {
-        $errores = "Debe contener al menos una letra mayúscula";
+    if (!preg_match('/[A-Z]/', $contrasena)) {
+        $errores .= "Debe contener al menos una letra mayúscula. ";
     }
-    elseif (!preg_match('/\d/', $contrasena)) {
-        $errores = "Debe contener al menos un número";
+    if (!preg_match('/\d/', $contrasena)) {
+        $errores .= "Debe contener al menos un número. ";
     }
 
     return empty($errores);
 }
 
-$contrasena = "Ejemplooo";
+$contrasena = "Ejeooo";
 if (validar_contrasena($contrasena, $errores)) {
     echo "Contraseña válida";
 } else {
     echo "Contraseña inválida: $errores";
 }
-
+?>
 

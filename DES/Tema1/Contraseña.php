389e712416fa2bf84bd0f72b69e9f0101aa57e31
Aitor <agj0034@alu.medac.es>
2025-09-23 16:08:32 +0200

    first commit

--- DES/Tema1/Contraseña.php
+++ DES/Tema1/Contraseña.php
@@ -1,14 +1,29 @@
 <?php
 // Función para validar la contraseña
-function validar_contrasena($contrasena) {
-    $patron = '/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/';
-    return preg_match($patron, $contrasena) === 1;
+function validar_contrasena($contrasena, &$errores) {
+    $errores = "";
+
+    if (strlen($contrasena) < 8) {
+        $errores = "Debe tener al menos 8 caracteres";
+    }
+    elseif (!preg_match('/[a-z]/', $contrasena)) {
+        $errores = "Debe contener al menos una letra minúscula";
+    }
+    elseif (!preg_match('/[A-Z]/', $contrasena)) {
+        $errores = "Debe contener al menos una letra mayúscula";
+    }
+    elseif (!preg_match('/\d/', $contrasena)) {
+        $errores = "Debe contener al menos un número";
+    }
+
+    return empty($errores);
 }
 
-$contrasena = "Ejemplo123";
-if (validar_contrasena($contrasena)) {
+$contrasena = "Ejemplooo";
+if (validar_contrasena($contrasena, $errores)) {
     echo "Contraseña válida";
 } else {
-    echo "Contraseña inválida";
+    echo "Contraseña inválida: $errores";
 }
-?>
+
+

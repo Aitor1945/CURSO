e704bb5eca502514ea0112368a250990e83363bc
Aitor <agj0034@alu.medac.es>
2025-09-23 15:10:18 +0200

    first commit

--- DES/Tema1/terminal.php
+++ DES/Tema1/terminal.php
@@ -1,11 +1,11 @@
 <?php
 function tablaMultiplicar($N) {
-    echo "Tabla de multiplicar del $N";
+    echo "Tabla de multiplicar del $N" . "\n";
     for ($i = 1; $i <= 10; $i++) {
         echo "$N x $i = " . ($N * $i) . "\n";
     }
 }
 
-$N = 10;
+$N = 20;
 tablaMultiplicar($N);
 ?>
